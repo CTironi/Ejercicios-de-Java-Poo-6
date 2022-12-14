3078b07fecd9c106f3db9f16a089cf4d94b015e3
CTironi <tironi741@gmail.com>
2022-12-14 17:44:44 -0300

    Creamos la clase peliculas con los atributos nombre, director y año de extreno, su constructor y los respectivos getters and setters

---
+++ src/main/java/com/sofka/Peliculas.java
@@ -0,0 +1,48 @@
+package com.sofka;
+
+public class Peliculas {
+
+    public Peliculas(String nombre, String director, Integer extreno){
+
+        this.nombre = nombre;
+        this.director = director;
+        this.extreno = extreno;
+    }
+
+    private String nombre;
+    private String director;
+    private Integer extreno;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Integer getExtreno() {
+        return extreno;
+    }
+
+    public void setExtreno(Integer extreno) {
+        this.extreno = extreno;
+    }
+
+    @Override
+    public String toString() {
+        return "Peliculas{" +
+                "nombre='" + nombre + '\'' +
+                ", director='" + director + '\'' +
+                ", extreno=" + extreno +
+                '}';
+    }
+}
